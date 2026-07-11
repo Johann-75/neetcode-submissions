503b1cdeeebd623ac2246bb8420f1b59be4f2ee2
Johann Mathew Sajan <65858481+Johann-75@users.noreply.github.com>
2026-07-11 22:08:54 +0530

    Add: implement-prefix-tree - submission-4

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-4.py
@@ -0,0 +1,39 @@
+class TrieNode:
+    def __init__(self) -> None:
+        self.children = {}
+        self.isEndOfWord = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+
+        curr.isEndOfWord = True            
+            
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for char in word:
+            if char in curr.children:
+                curr = curr.children[char]
+            else:
+                break
+        return curr.isEndOfWord
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for char in prefix:
+            if char in curr.children:
+                curr = curr.children[char]
+                continue
+            else:
+                return False
+        return True
+        
